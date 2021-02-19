51cdc93b1102d5296b0d4cec8bb3721e59aa69a8
K VINAY KUMAR REDDY <kvinaykumarreddy1995@gmail.com>
2021-02-20 00:53:40 +0530

    added requirements and updated settings file

--- todo/settings.py
+++ todo/settings.py
@@ -9,8 +9,13 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
 from pathlib import Path
+import os
+import environ
+
+environ.Env.read_env()
+env = environ.Env(DEBUG=(bool, False),)
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +25,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'ztij2)yk((9)0aqx2ugzvvk$+p@71-62(++ze^nx5tlj8knv4@'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG')
 
 ALLOWED_HOSTS = []
 
